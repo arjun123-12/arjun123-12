5b1c75a805fe8b579d9edbdf6fb6b7c263bf41dd
arjun123-12 <jirwanarjun2301@gmail.com>
2024-09-06 20:49:42 +0530

    Add files via upload

---
+++ sand.js
@@ -0,0 +1,42 @@
+/*
+let num1="arjun"
+let num2="kana"
+
+
+
+
+function MyFunction() {
+
+  let num = 50;
+  console.log(num);
+  console.log(num1) 
+  let x,num,sum;
+
+  x=10
+  y=20
+
+  num=x/2;
+  sum=x+y;
+
+  console.log(num + "<br>" + sum);
+}
+MyFunction();  */
+
+/*
+console.log("arjun")
+function sum(a,b) {
+  console.log(a+b);
+} 
+
+function  countVowels(str) {
+  let count =0;
+  for(const char of str) {
+    if(char==a)
+    console.log(char);
+  }
+}  */
+
+let num = [2,3,4,5];
+num.forEach((num)=> {
+  console.log(num*num);
+})
